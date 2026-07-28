e9ff4dbc07ebadd148b948a8d404efd42c429d73
Maximilian Mahl <8823101+r3dpan@users.noreply.github.com>
2026-07-28 23:38:20 +0200

    Fix paths and methods

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,7 +9,7 @@ import (
 
 // Product variables
 var productName = "Go Descendence API-Server"
-var productVersion = "v0.0.1"
+var productBuild = "v0.0.1"
 var apiVersion = "v0.0.1"
 
 // Configuration variables
@@ -26,11 +26,13 @@ func main() {
 	descendenceMux := http.NewServeMux()
 
 	// Create new API server
-	descendenceAPI := api.NewAPIServer(productName, productVersion, apiVersion)
+	descendenceAPI := api.NewAPIServer(productName, productBuild, apiVersion)
 
 	// Create api handlers
-	descendenceMux.HandleFunc("/", descendenceAPI.RootHandler)
-	descendenceMux.HandleFunc("/healthz", descendenceAPI.HealthHandler)
+	// Rule: the most specific pattern always wins
+	// A path pattern ending with '/' matches all paths beginning with that string. {$} anchors end of path.
+	descendenceMux.HandleFunc("GET /{$}", descendenceAPI.RootHandler)
+	descendenceMux.HandleFunc("GET /healthz", descendenceAPI.HealthHandler)
 
 	// Startup server
 	// As server always returns error, when returning -> log it
